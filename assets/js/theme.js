6ebde0a164c213b2703e214c278f0a5ddc3605d6
Aman bhai <rohanrikwar9555@gmail.com>
2023-07-18 02:49:54 +0530

    mobile-nav

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,23 +1,20 @@
-
-const $html=document.documentElement
-const isdark=window.matchMedia("(prefers-color-scheme:dark)").matches
-console.log(isdark)
-let ispressed=false;
+const $html=document.documentElement;
+const isDrak=window.matchMedia("(prefers-color-scheme:dark)").matches
+console.log(isDrak)
 if(sessionStorage.getItem("theme")){
-    $html.dataset.theme=sessionStorage.getItem("theme");
-
+    $html.dataset.theme=sessionStorage.getItem("theme")
 }else{
-    $html.dataset.theme=isdark ? "dark": "light";
+    $html.dataset.theme=isDrak?"dark":"dark";
 }
+let ispresed=false;
 const changetheme=function(){
-ispressed=ispressed ? false:true;
-this.setAttribute("aria-pressed",ispressed)
-$html.setAttribute("data-theme",($html.dataset.theme==="light")? "dark" :"light");
-sessionStorage.setItem("theme",$html.dataset.theme);
+  ispresed=ispresed?false:true;
+  this.setAttribute("aria-pressed",ispresed);
+  $html.setAttribute("data-theme",($html.dataset.theme==="light")?"dark":"light");
+  sessionStorage.setItem("theme",$html.dataset.theme)
+
 }
-
-
-window.addEventListener("load",function(){
-    const $thmebtn=this.document.querySelector("[data-theme-btn]");
-    $thmebtn.addEventListener("click",changetheme);
+window.addEventListener("load",()=>{
+    const chnthm=document.querySelector("[data-theme-btn]")
+    chnthm.addEventListener("click",changetheme)
 })
